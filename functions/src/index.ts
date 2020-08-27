f5f7446afacaf4a5d34debaa05857896ef9d9157
Dylan Modell <dylan.modell.promo@gmail.com>
2020-08-26 17:26:44 -0700

    ✨ sendWelcomeEmail (not working)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,26 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import * as nodemailer from 'nodemailer'
 admin.initializeApp()
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+const gmailEmail = functions.config().gmail.email
+const gmailPassword = functions.config().gmail.password
+const mailTransport = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+})
+
+// Your company name to include in the emails
+const APP_NAME = '8Bit Habits'
 
 exports.deleteUser = functions.https.onCall((data, context) => {
   const uid = data
@@ -9,5 +29,38 @@ exports.deleteUser = functions.https.onCall((data, context) => {
     .auth()
     .deleteUser(uid)
     .then(() => functions.logger.log('Successfully deleted user ', uid))
-    .catch((error) => functions.logger.log('Error deleting user:', error))
+    .catch((err) => functions.logger.log('Error deleting user:', err))
 })
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+  // Create new user document
+  admin
+    .firestore()
+    .collection('users')
+    .doc(user.uid)
+    .set({})
+    .then(() => functions.logger.log(`Successfully created user doc for [${user.uid}]`))
+    .catch((err) => functions.logger.log(`Error creating new user [${user.uid}] doc: ${err}`))
+  //TODO: Send welcome email
+  //! Not working goddamnit
+  const email = user.email // The email of the user.
+  const displayName = user.displayName // The display name of the user.
+
+  if (email && displayName) return sendWelcomeEmail(email, displayName)
+  return
+})
+
+// Sends a welcome email to the given user.
+async function sendWelcomeEmail(email: string, displayName: string) {
+  const mailOptions: any = {
+    from: `${APP_NAME} <${gmailEmail}>`,
+    to: email,
+  }
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Welcome to ${APP_NAME}!`
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`
+  await mailTransport.sendMail(mailOptions)
+  functions.logger.log('New welcome email sent to:', email)
+  return null
+}
